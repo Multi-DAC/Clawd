a252325bb7c6338b0c0d6c2b51e69d1138cae249
Multi-DAC <multidimensionalaccesschannel@gmail.com>
2026-07-19 14:00:57 -0800

    Delete operations/setup-scripts directory
    Removed for clean test slate

--- operations/setup-scripts/launch-musicgen-uv.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-exec bash /mnt/c/Users/mercu/clawd/operations/setup-scripts/install-musicgen-uv.sh
--- operations/setup-scripts/launch-musicgen.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Self-contained launcher for install-musicgen.sh; invoked via nohup setsid.
-exec bash /mnt/c/Users/mercu/clawd/operations/setup-scripts/install-musicgen.sh
